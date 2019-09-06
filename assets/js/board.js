81dd73ed861dd9c4ec2552e859763e30f01ebcd6
vanessmgi <vaness.m@gmail.com>
2019-09-06 18:10:07 +0200

    Update

--- assets/js/board.js
+++ assets/js/board.js
@@ -22,23 +22,36 @@ class Board {
    }
 
    generateGreyCells() {
+      // Générer "this._greyBox" positions aléatoires (cellules grises)
+      // Afficher les cases grises
+
+      // Et dans code HTML, mettre en gris, les cellules correspondantes
       // for (let i = 0; i < this._greyBox; i++) {
       //    console.log(i);
       // }
       const cells = $(".cell");
       console.log(cells);
+   }
 
+   generateGreyCellsPosition() {
+      // Boucle de 0 à greyBox
+         // generer aleatoirement un x entre this._col
+         // generer aleatoirement un y entre this._row
+         // stocker la position (x, y) dans une variable this.greysCells
+   }
+
+   HTMLGreyCells() {
+      // Boucle de 0 à nombre de cellules grises
+         // trouver dans le code HTML, le td correspondant
+            // dans le code HTML, trouver un td qui a la même position que la cellule grise courante
+         // griser le td correspondant
+            // ajouter une classe "cellule-grise"
    }
 }
 
 const board = new Board(4, 4, 2);
 board.generateBoard();
 board.generateGreyCells();
-
-
-
-
-
 
 
 // function generateGreyCells(nbGreyCells) {
