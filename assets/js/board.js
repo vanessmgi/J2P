3c497d0155f2d515978957459178ebf4de564148
Thomas Dimnet <thomas.dimnet@gmail.com>
2019-10-03 20:27:49 +0200

    Création du mini jeu.

--- assets/js/board.js
+++ assets/js/board.js
@@ -26,6 +26,9 @@ class Board {
    generateGreyCells() {
       console.log(this._greyBox)
 
+      // Sélectionne un noeud html de façon aléatoire
+      // $('.cell').eq(1).addClass('cellGr');
+
       for (let i = 0; i < this._greyBox; i++) {
          // Récupérer le total de noeuds ayant la class .cell
          let $len = $(".cell").length;
@@ -48,19 +51,53 @@ class Board {
    }
 
    generatePlayerPosition() {
-      console.log(this._nbPlayerPosition);
+      let $len = $(".cell").length
+      let randomPosition = Math.floor(Math.random() * $len) + 1
 
-      for (let i = 0; i < this._nbPlayerPosition; i++) {
-         let $len = $(".cell").length;
-         let $randomPosition = Math.floor(Math.random() * $len) + 1;
-         $('.cell').eq($randomPosition).addClass('cellPP');
+      // console.log(randomPosition)s
+      // $('.cell').eq(randomPosition).addClass('cellPP')
+
+      const hasGreyClass = $('.cell')
+         .eq(randomPosition)
+         .attr('class')
+         .split(/\s+/)
+         .find(myClass => myClass === 'cellGr')
+
+      console.log(hasGreyClass)
+
+      if (!hasGreyClass) {
+         $('.cell').eq(2).addClass('cellPP');
          $('.cellPP').text('Joueur');
       }
+      // console.log(this._nbPlayerPosition);
+
+      // const myClasses = $('.cell').eq(1).attr('class').split(/\s+/);
+
+      // const hasClass = myClasses.find(myClass => myClass === 'cellGr')
+
+      // console.log(myClasses)
+      // console.log(hasClass)
+
+      // if (hasClass === 'cellGr') {
+      //    console.log('refait un tour de bouble')
+      // } else {
+      //    console.log('on est content')
+      // }
+
+      // $('.cell').eq(2).addClass('cellPP');
+      // $('.cellPP').text('Joueur');
+
+      // for (let i = 0; i < this._nbPlayerPosition; i++) {
+      //    let $len = $(".cell").length;
+      //    let $randomPosition = Math.floor(Math.random() * $len) + 1;
+      //    $('.cell').eq($randomPosition).addClass('cellPP');
+      //    $('.cellPP').text('Joueur');
+      // }
    }
 
 }
 
-const board = new Board(8, 8, 4, 4, 2);
+const board = new Board(8, 8, 40, 4, 2);
 board.generateBoard();
 board.generateGreyCells();
 board.generateWeapons();
---
+++ mini-game/consignes.md
@@ -0,0 +1,9 @@
+# Todo
+
+1. Gérer le déplacement
+- Via le JavaScript, gérer le déplacement tour par tour de l'élément player (le récupérer sur le dom, écouter les événements)
+- Penser à supprimer la class player et à l'ajouter là où ça a été cliqué.
+
+2. Gérer le tour par tour
+- Augmenter le nombre de cases (4*4)
+- Via une variable, gérer le changement de tour (un coup player1, un coup player2)
