b32a646d74be8cd60ee52a4430761023ef21c370
vanessmgi <vaness.m@gmail.com>
2019-12-12 17:59:14 +0100

    Update script

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,50 @@
+class Game {
+    constructor(col, row, nbGreyCell, nbWeapon, nbPlayer) {
+        this.nbPlayer = nbPlayer;
+        this.board = new Board(col, row, nbGreyCell, nbWeapon, nbPlayer);
+        this.players = [];
+    };
+
+    start() {
+        this.board.generateBoard();
+        this.board.generateGreyCells();
+        this.board.generateWeapons();
+        this.board.generatePlayerPosition();
+        this.initPlayers();
+    };
+
+    initPlayers() {
+        for(let i=0; i<this.nbPlayer; i++) {
+            // this.players.push(new Player(100, new Weapon()));
+        };
+    };
+
+    getPlayer(indexPlayer) {
+        return this.players[indexPlayer];
+    };
+
+    getPlayerCell(indexPlayer) {
+        return $(`.cell[data-player-id=${indexPlayer}]`);
+    };
+
+    // attackPhase() {
+    //     this.getPlayer(0).attack(this.getPlayer(1));
+    // }
+
+    // defendPhase() {
+    //     this.getPlayer(0).defend(this.getPlayer(1));
+    // }
+};
+
 $(function () {
     /* Default game variables */
     let nbColumns = 8;
     let nbRows = 8;
-    let nbGreyCells = 2;
-    let nbWeapons = 4;
-    let nbPlayers = 2;
+    let nbGreyCell = 8;
+    let nbWeapon = 4;
+    let nbPlayer = 2;
+    let game = new Game(nbColumns, nbRows, nbGreyCell, nbWeapon, nbPlayer);
 
-
-
-
-
-
-    generateBoard(nbColumns, nbRows);
-    generateGreyCells(nbGreyCells);
-    generateWeapons(nbWeapons);
-    generatePlayers(nbPlayers);
-
-
-
-    
+    game.start();
 });
+
