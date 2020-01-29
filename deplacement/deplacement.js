bed8f47eee03d9feed0e8ecd11a1525fcbe74165
vanessmgi <vaness.m@gmail.com>
2020-01-29 15:30:30 +0100

    add potentialsMoves()

--- deplacement/deplacement.js
+++ deplacement/deplacement.js
@@ -1,7 +1,85 @@
 
+// ------ Retourne la cellule des coordonnées (x,y) ------
+function getCell(x, y) {
+    return $(`.cell[data-x=${x}][data-y=${y}]`);
+};
+
+
+// ------ Définit les mouvements d'un jouer -------
+function movePlayer() {
+
+    // Je sélectionne l'emplacement de mon joueur
+    const $player = $('.player');
+
+    // Je sélectionne mes cellules
+    const $cells = $('.cell')
+
+    // Je bouge mon joueur
+    $player.removeClass('player')
+    if ($cells.hasClass('.player')) {
+        $cells.attr('data-empty', false)
+    } else {
+        $cells.attr('data-empty', true)
+    }
+    $selectedCell = $(this);
+    $selectedCell.attr('data-empty', false)
+    $selectedCell.addClass('player');
+
+    $('.selectable-cell').off('click');
+    $('.selectable-cell').removeClass('selectable-cell');
+
+    // Je calcule les nouveaux déplacements possibles à partir de la nouvelle position de mon joueur
+    potentialsMoves()
+}
+
+
+// ------ Future fonction qui permettra de définir les déplacements potentiels ------
+function potentialsMoves() {
+
+    // 1. Prendre les coordonnées du joueur à l'emplacement ACTUEL
+    const $positionPlayerX = parseInt($('.player').attr('data-x'));
+    const $positionPlayerY = parseInt($('.player').attr('data-y'));
+    console.log("Position x = " + $positionPlayerX + " | " + "Position y = " + $positionPlayerY);
+
+    // 2. Définir le nombre de mouvement maximum 
+
+    const $maxMovesX = 2;
+    const $maxMovesY = 1;
+
+    // 3. Boucler jusqu'à que ce le nombre de mouvements maximum soit atteint
+
+    for (let i = 1; i <= $maxMovesX; i++) {
+        let $cellLeft = getCell($positionPlayerX - i, $positionPlayerY).addClass('selectable-cell');
+        let $cellRight = getCell($positionPlayerX + i, $positionPlayerY).addClass('selectable-cell');
+        
+    }
+
+    for (let i = 1; i <= $maxMovesY; i++) {
+        let $cellDown = getCell($positionPlayerX, $positionPlayerY - i).addClass('selectable-cell');
+        let $cellUp = getCell($positionPlayerX, $positionPlayerY + i).addClass('selectable-cell');
+    }
+
+    // cible la class .selectable-cell et applique la fonction movePlayer au clic
+    $('.selectable-cell').click(movePlayer);
+    
+}
+
+potentialsMoves()
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
-Déplacement sur toutes les cases 
+Déplacement sur toutes les cases
 
 ---
 
@@ -19,19 +97,8 @@ $cells.click(function() {
 })
 */
 
-
-/**
- * Taches de cette semaine :
- *  -> rester sur ce modèle simplifié
- *  -> pouvoir se déplacer (en étant au centre), d'une case vers la droite, vers la gauche, vers le haut et vers le bas
- *  -> une fois que c'est ok, de deux cases.
- *  -> réfléchir à une fonction qui pourrait faire ça
- */
-
-
-
-// Deplacement
-
+// Deplacement en dur
+/*
 $firstSelectedCell = $('.cell[data-x="2"][data-y="2"]')
 
 $rightSelectedCell = $('.cell[data-x="3"][data-y="2"]')
@@ -46,46 +113,20 @@ $dbUpSelectedCell = $('.cell[data-x="2"][data-y="0"]')
 $downSelectedCell = $('.cell[data-x="2"][data-y="3"]')
 $dbDownSelectedCell = $('.cell[data-x="2"][data-y="4"]')
 
-/*
-$firstSelectedCell.click(function () {
-    const $player = $('.player')
-    $player.removeClass('player')
-    $firstSelectedCell.addClass('player')
-
-    console.log('first ok')
-})
 */
 
-function movePlayer() {
-    // Je sélectionne l'emplacement de mon joueur
-    const $player = $('.player')
-
-    // Je sélectionne mes céllules
-    const $cells = $('.cell')
-
-    // Je bouge mon joueur
-    $player.removeClass('player')
-        if ($cells.hasClass('.player')) {
-            $cells.attr('data-empty', false)
-        } else {
-            $cells.attr('data-empty', true)
-        }
-    $selectedCell = $(this);
-    $selectedCell.attr('data-empty', false)
-    $selectedCell.addClass('player');
-
-    // Je calcule les nouveaux déplacements possibles à partir de la nouvelle position de mon joueur
-    calculatePossibleMovements()
-}
-
-// Future fonction qui permettra de définir les déplacements potentiels
-function calculatePossibleMovements() {
-}
 
 
+// let $coordinateCell = $('.cell').click(function () {
+//     const $x = $(this).attr("data-x");
+//     const $y = $(this).attr("data-y");
+// })
 
 
+// $coordinateCell.click(movePlayer)
 
+
+/*
 $firstSelectedCell.click(movePlayer);
 $rightSelectedCell.click(movePlayer);
 $dbRightSelectedCell.click(movePlayer);
@@ -95,11 +136,13 @@ $upSelectedCell.click(movePlayer);
 $dbUpSelectedCell.click(movePlayer);
 $downSelectedCell.click(movePlayer);
 $dbDownSelectedCell.click(movePlayer);
+*/
 
 /**
  * Objectifs semaine du 26 janvier
  *  -> Maintenant que tu l'as fait en dur, réaliser les event listeners avec un système de boucle au lieu de le faire en dur
  *  -> Après le faire en fonction du déplacement du joueur (ça va être dans une fonction que tu vas appeler à chaque déplacement)
  */
+
 
 
