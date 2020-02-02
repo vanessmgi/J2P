ef9a319cda78820c543adbfd27b19ea488d351df
tdimnetsource <thomas.dimnet@gmail.com>
2020-02-02 11:32:37 +0100

    Update player position.

--- deplacement/deplacement.js
+++ deplacement/deplacement.js
@@ -10,14 +10,20 @@ function getCell(x, y) {
     return $(`.cell[data-x=${x}][data-y=${y}]`);
 };
 
+function getPlayerPosition() {
+    return {
+        $positionPlayerX: parseInt($($activePlayer).attr('data-x')),
+        $positionPlayerY: parseInt($($activePlayer).attr('data-y'))
+    }
+}
+
 
 // Définition des moves potentiels && inaccessibilité des cases grises
 function potentialsMoves() {
 
     // ------ 1. Prendre les coordonnées du joueur à l'emplacement ACTUEL ------ //
-    const $positionPlayerX = parseInt($($activePlayer).attr('data-x'));
-    const $positionPlayerY = parseInt($($activePlayer).attr('data-y'));
-    console.log("Position x = " + $positionPlayerX + " | " + "Position y = " + $positionPlayerY);
+    // MAINTENANT dans la fonction getPlayerPosition
+    // console.log("Position x = " + $positionPlayerX + " | " + "Position y = " + $positionPlayerY);
 
 
     // ------ 2. Définir le nombre de mouvement maximum ------ //
@@ -32,7 +38,7 @@ function potentialsMoves() {
 
     /* vérification case grise à gauche */
     for (let i = 1; i <= $maxMovesX; i++) {
-        let $cellLeft = getCell($positionPlayerX - i, $positionPlayerY).addClass('selectable-cell');
+        let $cellLeft = getCell(getPlayerPosition().$positionPlayerX - i, getPlayerPosition().$positionPlayerY).addClass('selectable-cell');
 
         if ($cellLeft.hasClass('greycell') || $cellLeft.hasClass('player1') || $cellLeft.hasClass('player2')) {
             $cellLeft.removeClass('selectable-cell');
@@ -41,7 +47,7 @@ function potentialsMoves() {
     }
     /* vérification case grise à droite */
     for (let i = 1; i <= $maxMovesX; i++) {
-        let $cellRight = getCell($positionPlayerX + i, $positionPlayerY).addClass('selectable-cell');
+        let $cellRight = getCell(getPlayerPosition().$positionPlayerX + i, getPlayerPosition().$positionPlayerY).addClass('selectable-cell');
 
         if ($cellRight.hasClass('greycell') || $cellRight.hasClass('player1') || $cellRight.hasClass('player2')) {
             $cellRight.removeClass('selectable-cell')
@@ -50,7 +56,7 @@ function potentialsMoves() {
     }
     /* vérification case grise en haut */
     for (let i = 1; i <= $maxMovesY; i++) {
-        let $cellUp = getCell($positionPlayerX, $positionPlayerY - i).addClass('selectable-cell');
+        let $cellUp = getCell(getPlayerPosition().$positionPlayerX, getPlayerPosition().$positionPlayerY - i).addClass('selectable-cell');
 
         if ($cellUp.hasClass('greycell') || $cellUp.hasClass('player1') || $cellUp.hasClass('player2')) {
             $cellUp.removeClass('selectable-cell')
@@ -59,7 +65,7 @@ function potentialsMoves() {
     }
     /* vérification case grise en bas */
     for (let i = 1; i <= $maxMovesY; i++) {
-        let $cellDown = getCell($positionPlayerX, $positionPlayerY + i).addClass('selectable-cell');
+        let $cellDown = getCell(getPlayerPosition().$positionPlayerX, getPlayerPosition().$positionPlayerY + i).addClass('selectable-cell');
 
         if ($cellDown.hasClass('greycell') || $cellDown.hasClass('player1') || $cellDown.hasClass('player2')) {
             $cellDown.removeClass('selectable-cell');
@@ -101,6 +107,20 @@ function movePlayer() {
     $('.selectable-cell').off('click');
     $('.selectable-cell').removeClass('selectable-cell');
 
+    isOnBattleMode($player1, $player2)
+
     potentialsMoves()
     
 }
+
+function isOnBattleMode($player1, $player2) {
+    // Est-ce que les joueurs sont en mode combat
+    // L'idée de cette fonction : checker si les deux joueurs sont côté à côté
+}
+
+/**
+ * Todo
+ *  -> Mode combat
+ *      - 1. Tu peux commencer par coder en dure une position et si c'est les deux joueurs sont dessus, tu rentres en mode combat
+ *      - 2. Si c'est trop simple de base en dure, vas-y directement en mode dynamique
+ */
