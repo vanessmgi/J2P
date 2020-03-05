ae4b7907cc49f23192dc9556ba3124110ad7ef3e
vanessmgi <vaness.m@gmail.com>
2020-03-05 23:35:41 +0100

    function attack()

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,12 +3,20 @@ class Player {
         this.id = id;
         this.name = name;
         this.health = 100;
-        this.weapon = Weapons[0];
+        this.weapon = Weapons.filter(weapon => weapon.name === 'Knife').shift()
         this.previousWeapon = null;
         this.damages = 0;
-        this.image = './assets/img/player' + this.id + '.jpg';
+        this.image = `./assets/img/player${this.id}.jpg`;
         this.x = x;
         this.y = y;
         this.shield = 0;
+
     }
+
+    attack(currentPlayer, otherPlayer) {
+        let attackResult = currentPlayer * (1 - otherPlayer);
+        return attackResult;
+    }
+
+
 }
